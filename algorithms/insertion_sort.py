80c71f3e88a95f32c823b9a08171a637268b26a3
kshitiz kamal <skilite007@gmail.com>
2020-04-12 22:06:03 +0530

    completed insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -6,19 +6,30 @@ def startInsertionSort(data, drawData,stepTime):
     colorData=['grey' for x in range(len(data))]
     for i in range(1,len(data)):
         key=data[i]
+        j=i-1
+
         colorData[i]='blue'
         drawData(data,colorData)
         sleep(stepTime)
-        colorData[i]='grey'
-        j=i-1
+        
         while j>=0 and data[j]>key:
+
             colorData[j]='white'
-            data[j+1]=data[j]
             drawData(data,colorData)
+            sleep(stepTime)
             colorData[j]='grey'
+
+            data[j+1]=data[j]
             j-=1
+
         data[j+1]=key
-        colorData[i-1]='green'
+
+        colorData[j+1]='red'
         drawData(data,colorData)
         sleep(stepTime)
+        colorData[j+1]='grey'
+        colorData[i]='grey'
+
+    colorData=['green' for x in range(len(data))]
+    drawData(data,colorData)
 
